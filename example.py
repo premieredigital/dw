dad5f1f7bf0d9f615a0384b984d27fb4fc5ffc31
Ubuntu <ubuntu@ip-172-30-1-178.us-west-1.compute.internal>
2020-05-04 02:41:41 +0000

    actual example

--- example.py
+++ example.py
@@ -3,12 +3,12 @@ from base import *
 # This example has a LIMIT applied, but this is only so it will run faster -- a LIMIT probably should not be applied.
 if __name__ == '__main__':
     Warehouse().run(
-        script_name = 'test_historical_prices',
+        script_name = 'historical_prices',
         sql = """SELECT DATE(CONVERT_TZ(NOW(), 'UTC', 'America/Los_Angeles')) AS date,
                    name, content_type_id, release_year, i.provider_id, platform_type_id,
                    territory_id, p.store_url, a.code, a.price, a.currency_code_id
                  FROM main_iteminstance i JOIN main_territorypricing p ON p.item_id=i.id JOIN main_territorypricingavail a ON a.tp_id=p.id
-                 WHERE content_type_id IN ('movie', 'movie bundle') LIMIT 5000""",
+                 WHERE content_type_id IN ('movie', 'movie bundle')""",
         sql_args = None,
         partition_on_field = 'date',
         incremental_field = 'date'
